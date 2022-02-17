b1b273f2bf72946243661273995d5d8849e8ccd3
Noor-26 <nk26982698@gmail.com>
2022-02-17 11:41:40 +0600

    total expense and balance value added

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 function docId (idName){
     return document.getElementById(idName)
 }
+function parse(intId){
+    return parseFloat(intId.value)
+}
 docId('calculate-btn').addEventListener('click', function () {
-    const income = docId('income')
-    const incomeValue = income.value
-    console.log(incomeValue)
+    const income = docId('income');
+    const totalExpenses = docId('total-ex')
+    const balance = docId('total-balance')
+    const warningEx = docId('warning-ex') 
+    let expenses = 0
 
+    const food = docId('food-ex');
+    const rent = docId('rent-ex');
+    const clothes = docId('clothes-ex');
+
+     expenses = parse(food) + parse(rent) + parse(clothes)
+    totalExpenses.innerText = expenses
+
+    balance.innerText = income.value - expenses
+
+   
 })
